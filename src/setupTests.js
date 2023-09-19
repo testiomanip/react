e83eefafb28c1b886e2c9711ac9d5903d282677c
testiomanip <prathimaaraghavendraa@gmail.com>
2023-09-19 18:52:39 +0530

    first

---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// "homepage": "https://testiomanip.github.io/test",
+
+// "predeploy": "npm run build",
+//     "deploy": "gh-pages -d build",
